667005725ac76cd616e8fab1393060d112d1dd30
Kirill <37121507+d2side@users.noreply.github.com>
2020-04-18 00:27:08 +0200

    AddRandomXiaomiTest was addded

---
+++ allegro-web-tests/src/test/java/pl/allegro/tests/AddRandomXiaomiTest.java
@@ -0,0 +1,77 @@
+package pl.allegro.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddRandomXiaomiTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    
+  }
+
+  @Test
+  public void testAddRandomXiaomi() throws Exception {
+    driver.get("https://allegro.pl/");
+    driver.findElement(By.name("string")).click();
+    driver.findElement(By.name("string")).clear();
+    driver.findElement(By.name("string")).sendKeys("xiaomi");
+    driver.findElement(By.name("string")).sendKeys(Keys.ENTER);
+    driver.findElement(By.xpath("//img[contains(@src,'https://a.allegroimg.com/s128/030058/9eb5ceba42a783c805ef2e7b53d3')]")).click();
+    driver.findElement(By.linkText("przejdź do koszyka")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
